7d95830378e282a5e480ed103b498a5f4bce44ee
robertomorano <roberto.morano@iesnervion.es>
2024-11-19 14:41:59 +0100

    termnado

--- Tema3/src/boletin4/Ej6.java
+++ Tema3/src/boletin4/Ej6.java
@@ -6,40 +6,42 @@ import java.util.Scanner;
 public class Ej6 {
 
 	public static void main(String[] args) {
-		//Crear matriz
+		// Crear matriz
 		int matriz[];
-		int longitud=0;
-		//frecuencia de suma
+		int longitud = 0;
+		// frecuencia de suma
 		int frecuencia;
-		//Pedir longitud
+		// Pedir longitud
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce una longitud");
 		longitud = sc.nextInt();
 		matriz = new int[longitud];
-		matriz=generar(matriz);
-		//Pedir frecuencia de suma
+		matriz = generar(matriz);
+		// Pedir frecuencia de suma
 		System.out.println("Introduce el rango de suma");
 		frecuencia = sc.nextInt();
 		matriz = suma(matriz, frecuencia);
 		System.out.println("Se ha encontrado en: " + Arrays.toString(matriz));
 	}
+
 	static int[] suma(int t[], int numElementos) {
-		
-		int sum[] = new int [(t.length-(numElementos-1))];
-		for (int i=0; i<=t.length-numElementos;i++) {
-			for(int j=i; j<(i+numElementos);j++) {
-				sum[i]+=t[j];
+		// numElementos - 1 para ajustarr el tamaño de la matriz que sigue este algoritmo
+		int sum[] = new int[(t.length - (numElementos - 1))];
+		for (int i = 0; i <= t.length - numElementos; i++) {
+			for (int j = i; j < (i + numElementos); j++) {
+				sum[i] += t[j];
 			}
 		}
 		return sum;
 	}
+
 	static int[] generar(int t[]) {
 		for (int j = 0; j < t.length; j++) {
 			// Cambiar valor matriz
 			t[j] = (int) ((Math.random() * 10 + 1));
 			// Usando el tabulador le doy forma a la tabla
 			System.out.print(t[j] + " \t");
-			}
+		}
 		return t;
 	}
 
