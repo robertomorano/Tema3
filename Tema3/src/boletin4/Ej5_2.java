ce40abdcbe99d6f3bec8ed5c379b319c6df75ed0
robertomorano <roberto.morano@iesnervion.es>
2024-11-20 10:19:10 +0100

    giro90 terminado

--- Tema3/src/boletin4/Ej5_2.java
+++ Tema3/src/boletin4/Ej5_2.java
@@ -1,26 +1,30 @@
 package boletin4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ej5_2 {
 
 	public static void main(String[] args) {
 		// devuelve true
-		int matriz[][] = { { 4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } };
+		int matriz[][];
 		int longitud = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce una longitud para una matriz cuadrada");
 		longitud = sc.nextInt();
 		matriz = generar(longitud);
-		
+		matriz=giro90(matriz);
+		System.out.println(Arrays.deepToString(matriz));
 	}
 	static int[][] giro90(int t[][]){
+		int tCopia[][]= new int [t.length][t.length];
 		//patron que sigue j=i primera=ultima primera+1=ultima-1
 		for(int i=0;i<t.length;i++) {
 			for(int j=0;j<t.length;j++) {
-				
+				tCopia[j][(t.length-1)-i]=t[i][j];
 			}
 		}
+		return tCopia;
 	}
 
 	static int[][] generar(int longitud) {
